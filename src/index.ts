4734d048b1882669a700bce748b0a6f2ded8835e
JackieLi565 <li.jackie565@gmail.com>
2024-04-09 15:26:44 -0400

    fix package json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { RequestMethodMap, RequestMethods } from "./types.js";
 
 class Handler {
